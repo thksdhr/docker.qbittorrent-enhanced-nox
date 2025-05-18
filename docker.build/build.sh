fe6e74cdc26a1eff58441d2d5125f90a6e9acc1f
thksdhr@icloud.com <thksdhr@gmail.com>
2025-05-18 19:05:46 +0800

    修改 Actions 流程

--- docker.build/build.sh
+++ docker.build/build.sh
@@ -5,4 +5,4 @@ SHELL_DIR=$(dirname $SHELL_PATH)
 echo "SHELL_DIR=[$SHELL_DIR]"
 cd "$SHELL_DIR" || { echo "切换工作目录失败"; exit 1; }
 
-docker build -t qbittorrent-nox:latest -f ./dockerfile --no-cache --pull ./
+docker build -t taozig/qbittorrent-enhanced-nox:latest -f ./dockerfile --no-cache --pull ./
