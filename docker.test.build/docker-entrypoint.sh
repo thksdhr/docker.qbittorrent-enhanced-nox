8c594385ccbe6a8a31636cc2f1f47e236a381017
thksdhr@icloud.com <thksdhr@gmail.com>
2025-05-19 23:58:00 +0800

    新增测试Actions

---
+++ docker.test.build/docker-entrypoint.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+# FUNC
+# 检测是否转换 INT 类型成功
+function INT_CHECK() {
+    if [ "$#" -lt 1 ]; then
+        echo "错误: 需要至少一个参数。"
+        echo "用法: INT_CHECK <value>"
+        return 1 # 返回非零值表示失败
+    fi
+
+    if [[ $2 =~ ^-?[0-9]+$ ]]; then
+        echo "当前 $1 = $2"
+    else
+        echo "请检查输入的值是否为整数"
+        echo "exiting..."
+        exit 1
+    fi
+}
+
+# 检测环境变量是否正常
+echo "检测环境变量是否正确设置..."
+INT_CHECK WEBUI端口 ${WEBUI_PORT}
+INT_CHECK Torrenting端口 ${TORRENTING_PORT}
+
+# 启动 qBittorrent
+./qbittorrent-nox --confirm-legal-notice \
+    --webui-port=${WEBUI_PORT} \
+    --torrenting-port=${TORRENTING_PORT} \
+    --profile=${CONF_PATH} \
+    --configuration=${CONF_NAME} \
+    --relative-fastresume
---
+++ docker.test.build/install.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+
+echo "Starting install script"
+
+mkdir -p /install-temp
+cd /install-temp
+
+# qBittorrent Enhanced Edition
+# QBT_RELEASE_TAG=$(curl --silent "https://api.github.com/repos/c0re100/qBittorrent-Enhanced-Edition/releases/latest" | awk -F\" '/^ *"tag_name": "/ {print $4; exit}')
+# QBT_DOWNLOAD_URL="https://github.com/c0re100/qBittorrent-Enhanced-Edition/releases/download/${QBT_RELEASE_TAG}/qbittorrent-enhanced-nox_x86_64-linux-musl_static.zip"
+QBT_DOWNLOAD_URL="https://github.com/c0re100/qBittorrent-Enhanced-Edition/actions/runs/15096120900/artifacts/3146816611"
+curl -C - -LO# ${QBT_DOWNLOAD_URL}
+# Unzip the qBittorrent package
+unzip -o qbittorrent-enhanced-nox_x86_64-linux-musl_static.zip -d /programs
+
+# WebUI
+UI_RELEASE_TAG=$(curl --silent "https://api.github.com/repos/VueTorrent/VueTorrent/releases/latest" | awk -F\" '/^ *"tag_name": "/ {print $4; exit}')
+UI_DOWNLOAD_URL="https://github.com/VueTorrent/VueTorrent/releases/download/${UI_RELEASE_TAG}/vuetorrent.zip"
+curl -C - -LO# ${UI_DOWNLOAD_URL}
+# Unzip the WebUI package
+unzip -o vuetorrent.zip -d /programs
+
+# MiniConada
+curl -C - -LO# "https://repo.anaconda.com/miniconda/Miniconda3-latest-Linux-x86_64.sh"
+bash Miniconda3-latest-Linux-x86_64.sh -b -u -p /root/miniconda3
+source ~/miniconda3/bin/activate
+conda init --all
+# PATH="/root/miniconda3/bin:$PATH"
+
+# Cleanup
+cd / && rm -rf /install-temp
+
+# END
+echo "Install complete"
