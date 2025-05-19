b5e906ed28f2549e7c6dafc076d6cd63f4d39f32
thksdhr@icloud.com <thksdhr@gmail.com>
2025-05-20 07:34:42 +0800

    更改测试安装脚本

--- docker.test.build/install.sh
+++ docker.test.build/install.sh
@@ -9,14 +9,14 @@ cd /install-temp
 # QBT_RELEASE_TAG=$(curl --silent "https://api.github.com/repos/c0re100/qBittorrent-Enhanced-Edition/releases/latest" | awk -F\" '/^ *"tag_name": "/ {print $4; exit}')
 # QBT_DOWNLOAD_URL="https://github.com/c0re100/qBittorrent-Enhanced-Edition/releases/download/${QBT_RELEASE_TAG}/qbittorrent-enhanced-nox_x86_64-linux-musl_static.zip"
 QBT_DOWNLOAD_URL="https://github.com/c0re100/qBittorrent-Enhanced-Edition/actions/runs/15096120900/artifacts/3146816611"
-curl -C - -LO# ${QBT_DOWNLOAD_URL}
+curl -C - -L# -o qbittorrent-enhanced-nox_x86_64-linux-musl_static.zip ${QBT_DOWNLOAD_URL}
 # Unzip the qBittorrent package
 unzip -o qbittorrent-enhanced-nox_x86_64-linux-musl_static.zip -d /programs
 
 # WebUI
 UI_RELEASE_TAG=$(curl --silent "https://api.github.com/repos/VueTorrent/VueTorrent/releases/latest" | awk -F\" '/^ *"tag_name": "/ {print $4; exit}')
 UI_DOWNLOAD_URL="https://github.com/VueTorrent/VueTorrent/releases/download/${UI_RELEASE_TAG}/vuetorrent.zip"
-curl -C - -LO# ${UI_DOWNLOAD_URL}
+curl -C - -L# -o vuetorrent.zip ${UI_DOWNLOAD_URL}
 # Unzip the WebUI package
 unzip -o vuetorrent.zip -d /programs
 
