7c973d4c204cecc9a964d01e9462f2dc919ba6c0
thksdhr@icloud.com <thksdhr@gmail.com>
2025-05-20 00:04:28 +0800

    更新测试流程

--- docker.test.build/build_test.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-SHELL_PATH=$(readlink -f "${BASH_SOURCE[0]}")
-echo "SHELL_PATH=[$SHELL_PATH]"
-SHELL_DIR=$(dirname $SHELL_PATH)
-echo "SHELL_DIR=[$SHELL_DIR]"
-cd "$SHELL_DIR" || { echo "切换工作目录失败"; exit 1; }
-
-docker build -t taozig/qbittorrent-enhanced-nox:latest -f ./dockerfile --no-cache --pull ./
